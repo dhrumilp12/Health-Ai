3abf4c6681854308ec9f871ef025c3be6ce17069
DHRUMIL PATEL <dpatel24@radar.gsw.edu>
2025-02-15 22:44:03 -0500

    Update environment.prod.ts

--- Health_ai/src/app/shared/environments/environment.prod.ts
+++ Health_ai/src/app/shared/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    baseUrl: 'https://mememinglebackend.redrock-0e165097.eastus.azurecontainerapps.io', //development backend URL
+    baseUrl: 'https://health-ai-lped.onrender.com', //development backend URL
   };
